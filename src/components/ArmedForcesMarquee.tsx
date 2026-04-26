e27fa0dc46556f256279626471a31efb0c67a83f
boldmonk89 <tejascodes2006@gmail.com>
2026-04-26 15:34:56 +0530

    Fix: Replaced direct SVG links with reliable PNG thumbnails for Armed Forces emblems

--- src/components/ArmedForcesMarquee.tsx
+++ src/components/ArmedForcesMarquee.tsx
@@ -2,14 +2,38 @@ import LogoLoop from "./LogoLoop";
 
 const ArmedForcesMarquee = () => {
   const forces = [
-    { src: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Indian_Army_Emblem.svg", title: "Indian Army" },
-    { src: "https://upload.wikimedia.org/wikipedia/commons/5/53/Indian_Navy_Emblem.svg", title: "Indian Navy" },
-    { src: "https://upload.wikimedia.org/wikipedia/commons/d/df/Indian_Air_Force_Emblem.svg", title: "Indian Air Force" },
-    { src: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Indian_Coast_Guard_emblem.png", title: "Coast Guard" },
-    { src: "https://upload.wikimedia.org/wikipedia/en/3/37/National_Defence_Academy_%28India%29_logo.png", title: "NDA" },
-    { src: "https://upload.wikimedia.org/wikipedia/en/5/5b/Indian_Military_Academy_logo.png", title: "IMA" },
-    { src: "https://upload.wikimedia.org/wikipedia/en/e/e6/Indian_Air_Force_Academy_logo.png", title: "AFA" },
-    { src: "https://upload.wikimedia.org/wikipedia/en/0/09/Indian_Naval_Academy_logo.png", title: "INA" }
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Indian_Army_Emblem.svg/300px-Indian_Army_Emblem.svg.png", 
+      title: "Indian Army" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Indian_Navy_Emblem.svg/300px-Indian_Navy_Emblem.svg.png", 
+      title: "Indian Navy" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Indian_Air_Force_Emblem.svg/300px-Indian_Air_Force_Emblem.svg.png", 
+      title: "Indian Air Force" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Indian_Coast_Guard_emblem.png/300px-Indian_Coast_Guard_emblem.png", 
+      title: "Coast Guard" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/en/thumb/3/37/National_Defence_Academy_%28India%29_logo.png/300px-National_Defence_Academy_%28India%29_logo.png", 
+      title: "NDA" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5b/Indian_Military_Academy_logo.png/300px-Indian_Military_Academy_logo.png", 
+      title: "IMA" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/Indian_Air_Force_Academy_logo.png/300px-Indian_Air_Force_Academy_logo.png", 
+      title: "AFA" 
+    },
+    { 
+      src: "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/Indian_Naval_Academy_logo.png/300px-Indian_Naval_Academy_logo.png", 
+      title: "INA" 
+    }
   ];
 
   return (
@@ -22,11 +46,9 @@ const ArmedForcesMarquee = () => {
       <LogoLoop 
         logos={forces}
         speed={40}
-        logoHeight={60}
-        gap={80}
-        fadeOut
-        fadeOutColor="#050505"
-        pauseOnHover
+        logoHeight={50}
+        gap={120} // Space them out more for clarity
+        direction="left"
       />
     </div>
   );
